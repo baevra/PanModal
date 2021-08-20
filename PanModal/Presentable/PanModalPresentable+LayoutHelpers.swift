39440264fa5123c3c04aefdf6240fba8f8b58234
Roman Baev <baevra@gmail.com>
2021-08-20 18:35:38 +0700

    Update rootViewController

--- PanModal/Presentable/PanModalPresentable+LayoutHelpers.swift
+++ PanModal/Presentable/PanModalPresentable+LayoutHelpers.swift
@@ -109,6 +109,9 @@ extension PanModalPresentable where Self: UIViewController {
     }
 
     private var rootViewController: UIViewController? {
+        if let navigationController = presentingViewController as? UINavigationController {
+          return navigationController.visibleViewController
+        }
         return presentingViewController
     }
 
